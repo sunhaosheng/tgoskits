6dee23287197c842f752117455394af1d2b31d45
周睿 <zrufo747@outlook.com>
2026-04-27 14:48:02 +0800

    feat: add IRQ number support for console and timer interfaces

--- platform/axplat-dyn/src/generic_timer.rs
+++ platform/axplat-dyn/src/generic_timer.rs
@@ -33,6 +33,7 @@ impl ax_plat::time::TimeIf for GenericTimer {
         0
     }
     /// Returns the IRQ number for the timer interrupt.
+    #[cfg(feature = "irq")]
     fn irq_num() -> usize {
         somehal::irq::systick_irq().into()
     }
@@ -40,6 +41,7 @@ impl ax_plat::time::TimeIf for GenericTimer {
     ///
     /// A timer interrupt will be triggered at the specified monotonic time
     /// deadline (in nanoseconds).
+    #[cfg(feature = "irq")]
     fn set_oneshot_timer(deadline_ns: u64) {
         let cnptct = somehal::timer::ticks() as u64;
         let deadline = GenericTimer::nanos_to_ticks(deadline_ns);
